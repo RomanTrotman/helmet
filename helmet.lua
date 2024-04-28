7be2dfa519b81b7b3059cdccc803ef94b4b4ebbe
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-04-28 18:59:39 +0100

    Update helmet.lua

--- helmet.lua
+++ helmet.lua
@@ -9,6 +9,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Brief
 repeat task.wait(0.025) until Workspace:FindFirstChild(game:GetService("Players").LocalPlayer.Name)
 --  
 wait(4)
+
 local plr = game:GetService("Players").LocalPlayer
 local char = plr.Character
 local Map = game:GetService("Workspace").Map
@@ -18,41 +19,70 @@ local KC = CRoom.KeycardSpawns:FindFirstChild("Keycard")
 local Spawn = Instance.new("Part")
 Spawn.Position = char.Head.Position
 Spawn.Anchored = true
-Spawn.CanCollide = false
+local Noclip = nil
+local Clip = nil
+
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+noclip() -- to toggle noclip() and clip()
+local bodyVelocity = Instance.new("BodyVelocity")
+               bodyVelocity.Name = "float"
+               bodyVelocity.Parent = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 wait(1)
-while CRoom.KeycardSpawns:FindFirstChild("Keycard") do
-char.HumanoidRootPart.Position = KC.Handle.Position
-wait(1)
+while CRoom.KeycardSpawns:FindFirstChild("Keycard") ~= nil do
+    task.wait(0.2)
+if CRoom.KeycardSpawns:FindFirstChild("Keycard") then
+char:MoveTo(KC.Handle.Position)
 fireproximityprompt(KC.Base.GrabPrompt)
-wait(1)
-char.HumanoidRootPart.Position = char.Torso.Position  
 end
-wait(1)
-while Objective.ControlLever:FindFirstChild("Handle") do
-char.HumanoidRootPart.Position = Objective.ControlLever.Handle.Position
-wait(1)
-fireproximityprompt(workspace.Map.Objectives.ControlLever.Handle.ProximityPrompt)
-wait(1)
-char.HumanoidRootPart.Position = char.Torso.Position 
 end
+wait(0.2)
+char:MoveTo(Spawn.Position) 
 wait(1)
-while Objective.Radio:FindFirstChild("Handle") do
-char.HumanoidRootPart.Position = Objective.Radio.Handle.Position
+while workspace.Map.Objectives.ControlLever.Handle.ProximityPrompt.Enabled == true do
+    task.wait(0.2)
+if workspace.Map.Objectives.ControlLever:FindFirstChild("Handle") then 
+  char:MoveTo(Objective.ControlLever.Handle.Position)
+        fireproximityprompt(workspace.Map.Objectives.ControlLever.Handle.ProximityPrompt)
+end
+end
+wait(0.2)
+char:MoveTo(Spawn.Position)
 wait(1)
+while Objective.Radio:FindFirstChild("Handle"):FindFirstChild("ProximityPrompt") ~= nil do
+    task.wait(0.2)
+if Objective.Radio:FindFirstChild("Handle"):FindFirstChild("ProximityPrompt") then
+    char:MoveTo(Objective.Radio.Handle.Position)
 fireproximityprompt(game:GetService("Workspace").Map.Objectives.Radio.Handle.ProximityPrompt)
-wait(1)
-char.HumanoidRootPart.Position = char.Torso.Position
 end
-wait(1)
-char.HumanoidRootPart.Position = Objective.Radar.Hint.Position
+end
+wait(0.2)
+char:MoveTo(Spawn.Position)
 wait(1)
 for _,es in pairs(Objective.Radar:GetChildren()) do
     if es.Name == "Explosive1" or es.Name == "Explosive2" then
-        char.HumanoidRootPart.Position = es.Handle.Position
-        wait(1)
+        while es:FindFirstChild("Handle") ~= nil do
+            task.wait(0.2)
+if  es:FindFirstChild("Handle") ~= nil then
+ char:MoveTo(es.Handle.Position)
         fireproximityprompt(es:FindFirstChild("Handle").ProximityPrompt)
-        wait(1)
-        char.HumanoidRootPart.Position = char.Torso.Position
+        end
+end
+        wait(0.2)
+        char:MoveTo(Spawn.Position)
 
     end
 end
@@ -61,3 +91,4 @@ char:MoveTo(Objective.EscapeZone.Position)
 wait(3)
 queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/RomanTrotman/helmet/main/helmet.lua'))()")
 game:GetService("ReplicatedStorage").Remotes.Teleport.Replay:InvokeServer()
+
